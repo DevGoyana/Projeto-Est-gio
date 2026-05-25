023bdbb7e5e8537f74900c7c7cf0a52f23509bd5
Goyana-Dev <isaacbarreto011@gmail.com>
2026-05-25 11:47:29 -0300

    Ajuste das logos dos cursos. adição da aba Login

---
+++ ADMIN/JS(ADMIN)/admin.js
@@ -0,0 +1,48 @@
+let administradores = [];
+
+function cadastrarAdministrador(){
+
+    let usuario =
+    document.getElementById("usuario").value;
+
+    let senha =
+    document.getElementById("senha").value;
+
+    let mensagem =
+    document.getElementById("mensagem");
+
+    // VALIDAR CAMPOS
+    if(usuario === "" || senha === ""){
+
+        mensagem.innerText =
+        "Preencha todos os campos";
+
+        return;
+    }
+
+    // VERIFICAR SE USUÁRIO JÁ EXISTE
+    for(let i = 0; i < administradores.length; i++){
+
+        if(administradores[i].usuario === usuario){
+
+            mensagem.innerText =
+            "Usuário já cadastrado";
+
+            return;
+        }
+    }
+
+    // CADASTRAR
+    administradores.push({
+
+        usuario: usuario,
+
+        senha: senha
+    });
+
+    // MENSAGEM SUCESSO
+    mensagem.innerText =
+    `Usuário ${usuario} cadastrado com sucesso`;
+
+    mensagem.style.color = "green";
+}
